b595dad791fdf90fc220b91ad9376b068479141c
Jeffrey <afunpub@gmail.com>
2021-11-07 20:37:00 +0800

    ADD:Viewers

--- src/conponents/Header.js
+++ src/conponents/Header.js
@@ -76,7 +76,7 @@ const NavMenu = styled.div`
         bottom: -6px;
         opacity: 0;
         transform-origin: left center;
-        transition: all 250 ms ease 0s;
+        transition: all 250ms ease 0s;
         // transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
         transform: scaleX(0);
       }
